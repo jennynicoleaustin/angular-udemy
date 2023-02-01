e9d6af6ababa34f06227e73374ca960ac4af7475
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2023-02-01 16:30:29 -0600

    Section 7: Directives -> V 97 HostBinding -- even easier way

--- S7Directives/src/app/directive/better-highlight.directive.ts
+++ S7Directives/src/app/directive/better-highlight.directive.ts
@@ -1,9 +1,12 @@
-import {Directive, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  // on the element where this directive is used, access the style.background property
+  @HostBinding('style.backgroundColor') backgroundColor:string = 'transparent';
+
   // You will need an element ref as well as the renderer
   constructor(private elementRef: ElementRef, private renderer:Renderer2) { }
 
@@ -16,10 +19,18 @@ export class BetterHighlightDirective implements OnInit {
 
   // Reactive Directive - reacting to events
   @HostListener('mouseenter') mouseOver(eventData:Event) {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'orange');
+    // Without use of HostBinding
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'orange');
+  //   HostBinding example
+    this.backgroundColor = 'orange'
   }
   @HostListener('mouseleave') mouseLeave(eventData:Event) {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    // Without use of HostBinding
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+  //   HostBinding example
+    this.backgroundColor = 'transparent';
   }
+
+
 
 }
