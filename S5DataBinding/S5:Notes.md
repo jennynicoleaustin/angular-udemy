cc17d871f6da7e14bb3850ae3648e9c7b99f60a7
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2023-02-01 11:54:35 -0600

    Section 5: Components and Data Binding -> Assign Alias

---
+++ S5DataBinding/S5:Notes.md
@@ -0,0 +1,3 @@
+By default, all properties of components are only available within that component
+- must explicitly state decorator 
+  - @Input('assign element alias')
--- S5DataBinding/src/app/landing/landing.component.ts
+++ S5DataBinding/src/app/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-landing',
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
+  newServerName = '';
+  newServerContent = '';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
