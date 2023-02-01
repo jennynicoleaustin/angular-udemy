887738e5636b09ddd5315edc0fed8468e614a315
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2023-01-31 18:24:47 -0600

    Create Recipe List format

--- courseProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ courseProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe("Margarita", "Hannah's favorite", "https://images.pexels.com/photos/2480828/pexels-photo-2480828.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")
+  ];
 
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
 }
