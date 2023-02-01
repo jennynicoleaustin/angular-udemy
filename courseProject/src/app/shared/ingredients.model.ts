e3820a493cf2b80cdbf8d5f4c50b80695549b5ef
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2023-01-31 19:06:19 -0600

    Section 3: Course Project - The Basics -> complete

---
+++ courseProject/src/app/shared/ingredients.model.ts
@@ -0,0 +1,5 @@
+export class Ingredient {
+  // Shorthand -> automatically creates the fields, and assigns the fields within the constructor based on the parameters passed.
+  constructor(public name:string, public amount: number) {}
+
+}
