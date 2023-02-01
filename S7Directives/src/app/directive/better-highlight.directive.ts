4ebcde9e47276e66c38b7f1ad1e1b43c716133be
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2023-02-01 16:37:18 -0600

    Section 7: Directives -> V 98 Binding to directive Properties

--- S7Directives/src/app/directive/better-highlight.directive.ts
+++ S7Directives/src/app/directive/better-highlight.directive.ts
@@ -1,36 +1,41 @@
-import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor: string = 'transparent'
+  @Input() highlightColor: string = 'orange'
   // on the element where this directive is used, access the style.background property
-  @HostBinding('style.backgroundColor') backgroundColor:string = 'transparent';
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
 
   // You will need an element ref as well as the renderer
-  constructor(private elementRef: ElementRef, private renderer:Renderer2) { }
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+  }
 
   // you need to access the nativeElement not just the outer element
 
   // Renderer.setStyle takes in the element ref and the style and value for the style
   ngOnInit() {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'orange');
+  //   example using inputs
+    this.backgroundColor = this.defaultColor;
   }
 
   // Reactive Directive - reacting to events
-  @HostListener('mouseenter') mouseOver(eventData:Event) {
+  @HostListener('mouseenter') mouseOver(eventData: Event) {
     // Without use of HostBinding
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'orange');
-  //   HostBinding example
-    this.backgroundColor = 'orange'
-  }
-  @HostListener('mouseleave') mouseLeave(eventData:Event) {
-    // Without use of HostBinding
-    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
-  //   HostBinding example
-    this.backgroundColor = 'transparent';
+    //   HostBinding example
+    this.backgroundColor = this.highlightColor;
   }
 
+  @HostListener('mouseleave') mouseLeave(eventData: Event) {
+    // Without use of HostBinding
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    //   HostBinding example
+    this.backgroundColor = this.defaultColor;
+  }
 
 
 }
