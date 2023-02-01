fa4573a25a070ad1dc7e3e9a7b2a25d91202317d
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2023-02-01 16:20:44 -0600

    Section 7: Directives -> V 94 Better Attribute Directive -> Renderer is better

--- S7Directives/src/app/app.module.ts
+++ S7Directives/src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
+import {BasicHighlightDirective} from "./directive/basic-highlight.directive";
+import { BetterHighlightDirective } from './directive/better-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule,
