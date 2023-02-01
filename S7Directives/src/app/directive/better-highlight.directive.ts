901e129c707e9533b8950c3db29db77b78511d2d
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2023-02-01 16:25:38 -0600

    Section 7: Directives -> V 96 HostListener

--- S7Directives/src/app/directive/better-highlight.directive.ts
+++ S7Directives/src/app/directive/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -11,6 +11,15 @@ export class BetterHighlightDirective implements OnInit {
 
   // Renderer.setStyle takes in the element ref and the style and value for the style
   ngOnInit() {
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'orange');
+  }
+
+  // Reactive Directive - reacting to events
+  @HostListener('mouseenter') mouseOver(eventData:Event) {
     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'orange');
   }
+  @HostListener('mouseleave') mouseLeave(eventData:Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+  }
+
 }
