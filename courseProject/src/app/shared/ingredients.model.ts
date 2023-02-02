db6d27eab6edbc0f588e7b1eb31e9dd9c0a1e9cf
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2023-02-01 18:06:29 -0600

    Fix Course Project and bootstrap issue

--- courseProject/src/app/shared/ingredients.model.ts
+++
@@ -1,5 +0,0 @@
-export class Ingredient {
-  // Shorthand -> automatically creates the fields, and assigns the fields within the constructor based on the parameters passed.
-  constructor(public name:string, public amount: number) {}
-
-}
