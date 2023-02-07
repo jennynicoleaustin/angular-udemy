930cf38d07556f29e24ef906be9cd4845215f2e3
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2023-02-07 14:12:09 -0600

    S11: Routing -> V 136 Passing Query Parameters & Fragments -- programmatically

--- S11Routing/src/app/home/home.component.ts
+++ S11Routing/src/app/home/home.component.ts
@@ -13,9 +13,9 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
-  onLoadServers() {
+  onLoadServers(id: number) {
     //some method you want to execute before you navigate to a new page.
-    this.router.navigate(["/users"])
+    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'})
   }
 
 }
