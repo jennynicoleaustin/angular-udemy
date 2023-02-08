2f4b983023393254722ce04cb0d01c2dfca720c3
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2023-02-08 11:21:13 -0600

    S11: Routing -> V 151 Passing Dynamic Data with the resolve Guard

--- S11Routing/src/app/servers/server/server.component.ts
+++ S11Routing/src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 
 import {ServersService} from '../servers.service';
-import {ActivatedRoute, Params, Router} from "@angular/router";
+import {ActivatedRoute, Data, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-server',
@@ -17,12 +17,20 @@ export class ServerComponent implements OnInit {
   }
 
   ngOnInit() {
-    const id = +this.route.snapshot.params['id']
-    this.server = this.serversService.getServer(id);
-    this.route.params
+    // ------- Get server without the resolver ----
+    // const id = +this.route.snapshot.params['id']
+    // this.server = this.serversService.getServer(id);
+    // this.route.params
+    //   .subscribe(
+    //     (params: Params) => {
+    //       this.server = this.serversService.getServer(+params['id'])
+    //     }
+    //   )
+  //   ------- Get server with a resolver
+    this.route.data
       .subscribe(
-        (params: Params) => {
-          this.server = this.serversService.getServer(+params['id'])
+        (data: Data) => {
+          this.server = data['server'];
         }
       )
   }
